9d91251ee1776ae434a4279dbee7b7629ae06072
RyanCastro1 <84323119+RyanCastro1@users.noreply.github.com>
2022-04-28 19:52:59 -0300

    Add files via upload

---
+++ Exercicio10.js
@@ -0,0 +1,11 @@
+function retornaNNumerosPares(n) {
+    let numerosPares = [];
+    for (let i = 0; numerosPares.length < n; i++) {
+        if (i % 2 == 0) {
+            numerosPares.push(i);
+        }
+    }
+    return numerosPares;
+}
+
+console.log(retornaNNumerosPares(5));
